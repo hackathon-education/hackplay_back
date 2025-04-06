6c4bd20e9dd1fddbf10e2c2fdf1f341e680aa8f3
dbstj589 <lysfox8@gmail.com>
2025-04-06 17:44:01 +0900

    add

---
+++ src/main/java/com/hackplay/hackplay/HackplayApplication.java
@@ -0,0 +1,13 @@
+package com.hackplay.hackplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackplayApplication.class, args);
+	}
+
+}
